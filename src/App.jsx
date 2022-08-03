55d0f0989b698415f84782286a01e65a4c4b03ab
reiko51 <71922155+reiko51@users.noreply.github.com>
2022-08-03 22:46:47 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export const App = () => {
+  return (
+    <div className="container">
+      <p className="title"> - Reiko's Original -</p>
+      <button> Fight !! </button>
+    </div>
+  );
+};
